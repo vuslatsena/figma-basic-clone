8342065c73bdcdb66ecde4da8fe629a3b68073e3
Vuslat S. Emre <vuslatsena23@gmail.com>
2024-03-11 11:27:09 +0300

    done file structure & shadcn

--- app/CollaborativeApp.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useOthers } from "@/liveblocks.config";
-
-export function CollaborativeApp() {
-  const others = useOthers();
-  const userCount = others.length;
-  return <div> There are {userCount} others user(s) online </div>;
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import {Room} from "./Room";
-import {CollaborativeApp} from "./CollaborativeApp";
-
 export default function Page() {
-  return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
-  );
+  return <h1 className="font-5xl text-white">Liveblocks Figma Clone </h1>;
 }
