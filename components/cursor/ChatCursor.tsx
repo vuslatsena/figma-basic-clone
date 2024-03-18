e6861283005d3469f377f9f1f6069483452b03e2
Vuslat S. Emre <vuslatsena23@gmail.com>
2024-03-18 09:32:32 +0300

    add cursors components

--- components/cursor/ChatCursor.tsx
+++ components/cursor/ChatCursor.tsx
@@ -1,36 +1,30 @@
-import { useUpdateMyPresence } from "@/liveblocks.config";
-import CursorSVG from "@/public/assets/CursorSVG";
 import { CursorChatProps, CursorMode } from "@/types/type";
-import React from "react";
+import CursorSVG from "@/public/assets/CursorSVG";
 
 /**
  * 
  * @returns ChatCursor
  */
 
-const ChatCursor = ({
-  cursor,
-  cursorState,
-  setCursorState,
-  updateMyPresence,
-}: CursorChatProps) => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    updateMyPresence({ message: event.target.value });
+const ChatCursor = ({ cursor, cursorState, setCursorState, updateMyPresence }: CursorChatProps) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    updateMyPresence({ message: e.target.value });
     setCursorState({
       mode: CursorMode.Chat,
       previousMessage: null,
-      message: event.target.value,
+      message: e.target.value,
     });
   };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
       setCursorState({
         mode: CursorMode.Chat,
+        // @ts-ignore
         previousMessage: cursorState.message,
         message: "",
       });
-    } else if (event.key === "Escape") {
+    } else if (e.key === "Escape") {
       setCursorState({
         mode: CursorMode.Hidden,
       });
@@ -41,31 +35,38 @@ const ChatCursor = ({
     <div
       className="absolute top-0 left-0"
       style={{
-        transform: "translatex(${cursor.x}.px) translateY(${cursor.y}.px)",
+        transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
       }}
     >
+      {/* Show message input when cursor is in chat mode */}
       {cursorState.mode === CursorMode.Chat && (
         <>
+          {/* Custom Cursor shape */}
           <CursorSVG color="#000" />
-          <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
-            {cursorState.previousMessage && (
-              <div>
-                {cursorState.previousMessage && (
-                  <div>{cursorState.previousMessage}</div>
-                )}
-                <input
-                  className="z-10 w-60 border-none bg-transparent text-white placeholder-blue-300 outline-none"
-                  autoFocus={true}
-                  onChange={handleChange}
-                  onKeyDown={handleKeyDown}
-                  placeholder={
-                    cursorState.previousMessage ? "" : "Type a message"
-                  }
-                  value={cursorState.message}
-                  maxLength={75}
-                />
-              </div>
-            )}
+
+          <div
+            className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white"
+            onKeyUp={(e) => e.stopPropagation()}
+            style={{
+              borderRadius: 20,
+            }}
+          >
+            {/**
+             * if there is a previous message, show it above the input
+             *
+             * We're doing this because when user press enter, we want to
+             * show the previous message at top and the input at bottom
+             */}
+            {cursorState.previousMessage && <div>{cursorState.previousMessage}</div>}
+            <input
+              className="z-10 w-60 border-none	bg-transparent text-white placeholder-blue-300 outline-none"
+              autoFocus={true}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+              placeholder={cursorState.previousMessage ? "" : "Say something…"}
+              value={cursorState.message}
+              maxLength={50}
+            />
           </div>
         </>
       )}
--- components/cursor/LiveCursors.tsx
+++ components/cursor/LiveCursors.tsx
@@ -1,20 +1,22 @@
+import Cursor from "./Cursor";
 import { COLORS } from "@/constants";
 import { LiveCursorProps } from "@/types/type";
-import React from "react";
-import Cursor from "./Cursor";
 
+// display all other live cursors
 const LiveCursors = ({ others }: LiveCursorProps) => {
   return others.map(({ connectionId, presence }) => {
-    if (!presence) return null;
+    if (presence == null || !presence?.cursor) {
+      return null;
+    }
 
     return (
       <Cursor
         key={connectionId}
         color={COLORS[Number(connectionId) % COLORS.length]}
         x={presence.cursor.x}
-        y={presence.cursor.y} 
-        message={presence.message}      
-        />
+        y={presence.cursor.y}
+        message={presence.message}
+      />
     );
   });
 };
